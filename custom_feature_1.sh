285b9bba64d0ce1be32ced0162658e9fc955bbae
Evan Mutic <emutic@wustl.edu>
2022-11-16 12:49:28 -0600

    commenting goals of get_seq.sh

---
+++ custom_feature_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'hello world'
+
+# this has ideas for the customer feature #1
+# and will eventually have the code
+
+
+# Ideas:
+#1. specify a gene and bacteria, pull down accession for those sequences, save information on the sequences
+# into text file in a dir in the project directory then format an accession.
--- get_seq.sh
+++ get_seq.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-echo 'hello world!'
+
+
+# gets fasta-formatted accessions from GenBank
+# accepts two arguments. $1 is a list of accessions
+# and a directory where the sequences are managed
+# ./get_seq ACCESSION_FILE SEQUENCE_DIR [OVERWRITE]
+accessions=$1
+dir=$2
+
+
+# check if the accession has been downloaded into the managed directory
+# download any missing sequences and append any issues to the file
+# warnings.log
+
+# for loop iterate over each line (accession number) in the accessions file
+# do
+# this will mark files to be downloaded
+#		if seq in dir
+			# a) if >description and SeqData exist in the accession
+				#continue
+			# b) if the seq is invalid
+				# delete the accession file and mark it to be downloaded
+				# record deleted files in the warnings.log
+			# c if OVERWRITE == true then treat  all sequences as missing accessions (put c first)
+# for loop to iterate over the file to be downloaded
+# do
+		# download the accession from genbank using equary and efetch and save as accession.fasta
+		# if the accession is invalid,report to the warnings.log which files weren't downloadedxy
+
