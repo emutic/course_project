0ac24625be4be520ad5b03b4cdd9db7288488226
Evan Mutic <emutic@wustl.edu>
2022-11-28 01:11:18 -0600

    mostly done with align ... have to add the warning log

--- make_align.sh
+++ make_align.sh
@@ -41,7 +41,7 @@ then
 		rm "./$sequence_dir/$sequence_dir.align_muscle.fasta"
 		touch "./$sequence_dir/$sequence_dir.align_muscle.fasta"
 	fi
-	muscle -in ./$sequence_dir/all_sequences  -out "./$sequence_dir/$sequence_dir.align_muscle.fasta" -maxiters 1
+	muscle -in ./$sequence_dir/all_sequences  -out "./$sequence_dir/$sequence_dir.align_muscle.fasta" -maxiters $maxiters
 	if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_muscle.log") ]]
 	then
 		echo "making alignment settings file"
@@ -52,7 +52,7 @@ then
 		touch "./$sequence_dir/$sequence_dir.align_muscle.log"
 	fi
 	echo "alignment file name: $sequence_dir.align_muscle.fasta" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
-	echo "command string: muscle -in ./$sequence_dir/all_sequences  -out './$sequence_dir/$sequence_dir.align_muscle.fasta' -maxiters 1" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
+	echo "command string: muscle -in ./$sequence_dir/all_sequences  -out \"./$sequence_dir/$sequence_dir.align_muscle.fasta\" -maxiters 1" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
 	date=$(date)
 	echo "alignment was created on $date" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
 	num_seq=$(cat "./$sequence_dir/all_sequences" | grep ">" | wc -l)
@@ -66,41 +66,77 @@ fi
 if [[ $align_tool = "MAFFT" ]]
 then
         echo "running MAFFT"
-	echo "running muscle"
-        maxiters=$(echo "$align_tool_options" | grep "Muscle" | cut -d "," -f2 | cut -d "=" -f2)
-        if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_muscle.fasta") ]]
+        op=$(echo "$align_tool_options" | grep "MAFFT" | cut -d "," -f2 | cut -d "=" -f2)
+	ep=$(echo "$align_tool_options" | grep "MAFFT" | cut -d "," -f3 | cut -d "=" -f2)
+	if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_MAFFT.fasta") ]]
         then
                 echo "making output file"
-                touch "./$sequence_dir/$sequence_dir.align_muscle.fasta"
+                touch "./$sequence_dir/$sequence_dir.align_MAFFT.fasta"
         else
                 echo "resetting output file"
-                rm "./$sequence_dir/$sequence_dir.align_muscle.fasta"
-                touch "./$sequence_dir/$sequence_dir.align_muscle.fasta"
+                rm "./$sequence_dir/$sequence_dir.align_MAFFT.fasta"
+                touch "./$sequence_dir/$sequence_dir.align_MAFFT.fasta"
         fi
-        muscle -in ./$sequence_dir/all_sequences  -out "./$sequence_dir/$sequence_dir.align_muscle.fasta" -maxiters 1
-        if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_muscle.log") ]]
+
+        mafft --op $op --ep $ep "./$sequence_dir/all_sequences" > "./$sequence_dir/$sequence_dir.align_MAFFT.fasta"
+        if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_MAFFT.log") ]]
         then
                 echo "making alignment settings file"
-                touch "./$sequence_dir/$sequence_dir.align_muscle.log"
+                touch "./$sequence_dir/$sequence_dir.align_MAFFT.log"
         else
                 echo "resetting the log file"
-                rm "./$sequence_dir/$sequence_dir.align_muscle.log"
-                touch "./$sequence_dir/$sequence_dir.align_muscle.log"
+                rm "./$sequence_dir/$sequence_dir.align_MAFFT.log"
+                touch "./$sequence_dir/$sequence_dir.align_MAFFT.log"
         fi
-        echo "alignment file name: $sequence_dir.align_muscle.fasta" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
-        echo "command string: muscle -in ./$sequence_dir/all_sequences  -out './$sequence_dir/$sequence_dir.align_muscle.fasta' -maxiters 1" >> >
+
+        echo "alignment file name: $sequence_dir.align_MAFFT.fasta" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
+        echo "command string: mafft --op $op --ep $ep \"./$sequence_dir/all_sequences\" > \"./$sequence_dir/$sequence_dir.align_MAFFT.fasta\"" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
         date=$(date)
-        echo "alignment was created on $date" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
+        echo "alignment was created on $date" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
         num_seq=$(cat "./$sequence_dir/all_sequences" | grep ">" | wc -l)
-        num_sites=$(cat "$sequence_dir/$sequence_dir.align_muscle.fasta" | grep -v ">" | wc -c)
+        num_sites=$(cat "$sequence_dir/$sequence_dir.align_MAFFT.fasta" | grep -v ">" | wc -c)
         num_sites=$(($num_sites/$num_seq))
-        echo "number of sequences aligned: $num_seq" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
-        echo "number of sites aligned: $num_sites" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
-        echo "$align_tool_options" | grep "Muscle" >> "./$sequence_dir/$sequence_dir.align_muscle.log"
+        echo "number of sequences aligned: $num_seq" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
+        echo "number of sites aligned: $num_sites" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
+        echo "$align_tool_options" | grep "MAFFT" >> "./$sequence_dir/$sequence_dir.align_MAFFT.log"
 
 fi
 
 if [[ $align_tool = "PRANK" ]]
 then
-        echo "PRANK detected"
+	echo "running PRANK"
+        gaprate=$(echo "$align_tool_options" | grep "PRANK" | cut -d "," -f2 | cut -d "=" -f2)
+        gapext=$(echo "$align_tool_options" | grep "PRANK" | cut -d "," -f3 | cut -d "=" -f2)
+	if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_PRANK.fasta") ]]
+        then
+                echo "making output file"
+                touch "./$sequence_dir/$sequence_dir.align_PRANK.fasta"
+        else
+                echo "resetting output file"
+                rm "./$sequence_dir/$sequence_dir.align_PRANK.fasta"
+                touch "./$sequence_dir/$sequence_dir.align_PRANK.fasta"
+        fi
+	prank -gaprate="$gaprate" -gapext="$gapext" -d="./$sequence_dir/all_sequences" -o="./$sequence_dir/$sequence_dir.align_PRANK.fasta"; cp "./$sequence_dir/$sequence_dir.align_PRANK.fasta.best.fas" "./$sequence_dir/$sequence_dir.align_PRANK.fasta"; rm "./$sequence_dir/$sequence_dir.align_PRANK.fasta.best.fas"
+        if [[ ! -n $(cat "./$sequence_dir/$sequence_dir.align_PRANK.log") ]]
+        then
+                echo "making alignment settings file"
+                touch "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        else
+                echo "resetting the log file"
+                rm "./$sequence_dir/$sequence_dir.align_PRANK.log"
+                touch "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        fi
+
+        echo "alignment file name: $sequence_dir.align_PRANK.fasta" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        echo "command string: prank -gaprate=\"$gaprate\" -gapext=\"$gapext\" -d=\"./$sequence_dir/all_sequences\" -o=\"./$sequence_dir/$sequence_dir.align_PRANK.fasta\"; cp \"./$sequence_dir/$sequence_dir.align_PRANK.fasta\"; rm \"./$sequence_dir/$sequence_dir.align_PRANK.fasta.best.fas\"" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        date=$(date)
+        echo "alignment was created on $date" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        num_seq=$(cat "./$sequence_dir/all_sequences" | grep ">" | wc -l)
+        num_sites=$(cat "$sequence_dir/$sequence_dir.align_PRANK.fasta" | grep -v ">" | wc -c)
+        num_sites=$(($num_sites/$num_seq))
+        echo "number of sequences aligned: $num_seq" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        echo "number of sites aligned: $num_sites" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+        echo "$align_tool_options" | grep "PRANK" >> "./$sequence_dir/$sequence_dir.align_PRANK.log"
+
+
 fi
