334432eeea5ccb12ef877830b17c48d7ad92d287
Evan Mutic <emutic@wustl.edu>
2022-11-28 10:26:53 -0600

    working on lab

--- make_phylo.sh
+++ make_phylo.sh
@@ -27,21 +27,22 @@ phylo_tool=$2
 phylo_tool_options=$(cat "$3")
 OUTGROUP=$4
 date=$(date)
-if [[ ! -n $OUTGROPU ]]
+if [[ ! -n $OUTGROUP ]]
 then
 	OUTGROUP=$(cat "./sequences/$align_file" | head -n1 | tr -d ">")
 fi
 
 if [[ $phylo_tool = FastTree ]]
 then
-	if [[ ! -n "./phylogenies/$align_file.phylo_fasttree.log"]]
+	if [[ ! -n "./phylogenies/$align_file.phylo_fasttree.log" ]]
         then
                 touch "./phylogenies/$align_file.phylo_fasttree.log"
         else
                 rm "./phylogenies/$align_file.phylo_fasttree.log"
                 touch "./phylogenies/$align_file.phylo_fasttree.log"
         fi
-	complexity=$(cat "$phylo_tool_options" | grep "FastTree" | cut -d "," -f2 | cut -d "=" -f2)
+
+	complexity=$(echo "$phylo_tool_options" | grep "FastTree" | cut -d "," -f2 | cut -d "=" -f2
 	if [[ -n $complexity || -n $(cat "./sequences/$align_file") ]]
 	then
 		if [[ ! -n $(cat "./phylogenies/$align_file.phylo_fasttree.tre") ]]
@@ -51,6 +52,7 @@ then
                 	rm "./phylogenies/$align_file.phylo_fasttree.tre"
                 	touch  "./phylogenies/$align_file.phylo_fasttree.tre"
         	fi
+		exit
 		echo "name and location of file: ./phylogenies/$align_file.phylo_fasttree.log" >> "./phylogenies/$align_file.phylo_fasttree.log"
 		if [[ $complexity = "complex" ]]
 		then
@@ -62,6 +64,7 @@ then
 		fi
 		echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_fasttree.log"
 	else
+		echo "warning: FastTree failed to run for $align_file" 
 		# print something to the warning log
 	fi
 fi
@@ -90,50 +93,44 @@ then
 			echo "name and location of newick string file: ./phylogenies/$align_file.phylo_IQ-Tree.tre" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
 			if [[ $complexity = "complex" ]]
 			then
-				iqtree -s "./sequences/$align_file" -m GTR JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree.tre"
-				echo "command string:  iqtree -s \"./sequences/$align_file\" -m GTR JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_IQ-Tree.tre\"" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
+				iqtree -s $(cat "./sequences/$align_file") -m GTR JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree.tre"
+				echo "command string:  iqtree -s $(cat \"./sequences/$align_file\") -m GTR JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_IQ-Tree.tre\"" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
 			else
-				iqtree -s "$alignment" -m JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree.tre"
-				echo "iqtree -s \"$alignment\" -m JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_IQ-Tree.tre\"" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
+				iqtree -s $(cat "./sequences/$align_file") -m JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree.tre"
+				echo "iqtree -s $(cat \"./sequences/$align_file\") -m JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_IQ-Tree.tre\"" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
 			fi
 		echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
 	else
+		echo "warning: IQ-Tree failed to run for $align_file" 
 		#print something to the warning log
 	fi
 fi
 
 if [[ $phylo_tool = MPBoot ]] # change everything to MPBoot instead of IQ-Tree
 then
-	if [[ ! -n "./phylogenies/$align_file.phylo_IQ-Tree.log"]]
+	if [[ ! -n "./phylogenies/$align_file.phylo_MPBoot.log"]]
         then
-                touch "./phylogenies/$align_file.phylo_IQ-Tree.log"
+                touch "./phylogenies/$align_file.phylo_MPBoot.log"
         else
-                rm "./phylogenies/$align_file.phylo_IQ-Tree.log"
-                touch "./phylogenies/$align_file.phylo_IQ-Tree.log"
+                rm "./phylogenies/$align_file.phylo_MPBoot.log"
+                touch "./phylogenies/$align_file.phylo_MPBoot.log"
         fi
-       # complexity=$(cat "$phylo_tool_options" | grep "IQ-Tree" | cut -d "," -f2 | cut -d "=" -f2)
-        if [[ -n $complexity || -n $(cat "./sequences/$align_file") ]]
-        then
-                if [[ ! -n $(cat "./phylogenies/$align_file.phylo_IQ-Tree.tre") ]]
-                then
-                        touch "./phylogenies/$align_file.phylo_IQ-Tree.tre"
+       # there are no settings for MPBoot
+        if [[ -n "./sequences/$align_file" ]]
+		if [[ ! -n $(cat "./phylogenies/$align_file.phylo_MPBoot.tre") ]]
+        	then
+			touch "./phylogenies/$align_file.phylo_MPBoot.tre"
                 else
-                        rm "./phylogenies/$align_file.phylo_IQ-Tree.tre"
-                        touch  "./phylogenies/$align_file.phylo_IQ-Tree.tre"
+                        rm "./phylogenies/$align_file.phylo_MPBoot.tre"
+                        touch  "./phylogenies/$align_file.phylo_MPBoot.tre"
                 fi
-                if [[ -n $complexity || -n $(cat "./sequences/$align_file") ]]
-                then
-                        echo "name and location of newick string file: ./phylogenies/$align_file.phylo_IQ-Tree.tre" >> "./phylogenies/$align_file.phyl>
-                        if [[ $complexity = "complex" ]]
-                        then
-           #                     iqtree -s "./sequences/$align_file" -m GTR JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree>
-                                echo "command string:  iqtree -s \"./sequences/$align_file\" -m GTR JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogeni>
-                        else
-          #                      iqtree -s "$alignment" -m JC+FQ -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_IQ-Tree.tre"
-                                echo "iqtree -s \"$alignment\" -m JC+FQ -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_IQ-Tree.tre\">
-                        fi
-                echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
+
+                        echo "name and location of newick string file: ./phylogenies/$align_file.phylo_MPBoot.tre" >> "./phylogenies/$align_file.phylo_MPBoot.log"
+		       	mpboot -s $(cat "./sequences/$align_file" -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_MPBoot.tre"
+			echo "command string: mpboot -s $(cat \"./sequences/$align_file\" -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_MPBoot.tre\"" >> "./phylogenies/$align_file.phylo_MPBoot.log"
+                	echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_MPBoot.log"
         else
+		echo "warning: MPBoot failed to run for $align_file"
                 #print something to the warning log
         fi
 fi
