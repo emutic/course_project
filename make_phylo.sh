963cc60941094c253abeff5f542f533757d2e200
Evan Mutic <emutic@wustl.edu>
2022-11-28 11:11:02 -0600

    working on lab trying to do phylo, go back and fix if, then, else, fi

--- make_phylo.sh
+++ make_phylo.sh
@@ -23,6 +23,7 @@
 
 
 align_file=$1
+align_seqs=$(cat "./sequences/$align_file")
 phylo_tool=$2
 phylo_tool_options=$(cat "$3")
 OUTGROUP=$4
@@ -31,10 +32,11 @@ if [[ ! -n $OUTGROUP ]]
 then
 	OUTGROUP=$(cat "./sequences/$align_file" | head -n1 | tr -d ">")
 fi
+# echo $align_file; echo $align_seqs; echo $phylo_tool; echo $phylo_tool_options; echo $OUTGROUP
 
-if [[ $phylo_tool = FastTree ]]
+if [[ $phylo_tool = "FastTree" ]]
 then
-	if [[ ! -n "./phylogenies/$align_file.phylo_fasttree.log" ]]
+	if [[ ! -n $(find "./phylogenies/$align_file.phylo_fasttree.log") ]]
         then
                 touch "./phylogenies/$align_file.phylo_fasttree.log"
         else
@@ -42,43 +44,44 @@ then
                 touch "./phylogenies/$align_file.phylo_fasttree.log"
         fi
 
-	complexity=$(echo "$phylo_tool_options" | grep "FastTree" | cut -d "," -f2 | cut -d "=" -f2
+	complexity=$(echo "$phylo_tool_options" | grep "FastTree" | cut -d "," -f2 | cut -d "=" -f2)
 	if [[ -n $complexity || -n $(cat "./sequences/$align_file") ]]
 	then
-		if [[ ! -n $(cat "./phylogenies/$align_file.phylo_fasttree.tre") ]]
+		if [[ ! -n $(find "./phylogenies/$align_file.phylo_fasttree.tre") ]]
         	then
                 	touch "./phylogenies/$align_file.phylo_fasttree.tre"
         	else
                 	rm "./phylogenies/$align_file.phylo_fasttree.tre"
                 	touch  "./phylogenies/$align_file.phylo_fasttree.tre"
         	fi
-		exit
-		echo "name and location of file: ./phylogenies/$align_file.phylo_fasttree.log" >> "./phylogenies/$align_file.phylo_fasttree.log"
+		echo "name and location of file: ./phylogenies/$align_file.phylo_fasttree.tre" >> "./phylogenies/$align_file.phylo_fasttree.log"
 		if [[ $complexity = "complex" ]]
 		then
-			fasttree -nt -gtr -noml $(cat "./sequences/$align_file") >> "./phylogenies/$align_file.phylo_fasttree.tre"
-			echo "command string: fasttree -nt -gtr -noml $(cat \"./sequences/$align_file\") >> \"./phylogenies/$align_file.phylo_fasttree.tre\"" >> "./phylogenies/$align_file.phylo_fasttree.log"
+			fasttree -nt -gtr -noml "./sequences/$align_file" >> "./phylogenies/$align_file.phylo_fasttree.tre"
+			echo "command string: fasttree -nt -gtr -noml \"./sequences/$align_file\" >> \"./phylogenies/$align_file.phylo_fasttree.tre\"" >> "./phylogenies/$align_file.phylo_fasttree.log"
 		else
-			fasttree -gtr -noml $(cat "./sequences/$align_file") >> "./phylogenies/$align_file.phylo_fasttree.tre"
-			echo "command string: fasttree -gtr -noml $(cat \"./sequences/$align_file\") >> \"./phylogenies/$align_file.phylo_fasttree.tre\"" >> "./phylogenies/$align_file.phylo_fasttree.log"
+			fasttree -gtr -noml "./sequences/$align_file" >> "./phylogenies/$align_file.phylo_fasttree.tre"
+			echo "command string: fasttree -gtr -noml \"./sequences/$align_file\" >> \"./phylogenies/$align_file.phylo_fasttree.tre\"" >> "./phylogenies/$align_file.phylo_fasttree.log"
 		fi
 		echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_fasttree.log"
 	else
-		echo "warning: FastTree failed to run for $align_file" 
+		echo "warning: FastTree failed to run for $align_file"
 		# print something to the warning log
 	fi
 fi
 
-if [[ $phylo_tool = IQ-Tree ]]
+if [[ $phylo_tool = "IQ-Tree" ]] # I think I have the if, then, else, fi messed up FIX
 then
-	if [[ ! -n "./phylogenies/$align_file.phylo_IQ-Tree.log"]]
+	echo "check"
+
+	if [[ ! -n $(find "./phylogenies/$align_file.phylo_IQ-Tree.log") ]]
         then
                 touch "./phylogenies/$align_file.phylo_IQ-Tree.log"
         else
                 rm "./phylogenies/$align_file.phylo_IQ-Tree.log"
                 touch "./phylogenies/$align_file.phylo_IQ-Tree.log"
         fi
-	complexity=$(cat "$phylo_tool_options" | grep "IQ-Tree" | cut -d "," -f2 | cut -d "=" -f2)
+	complexity=$(echo "$phylo_tool_options" | grep "IQ-Tree" | cut -d "," -f2 | cut -d "=" -f2)
 	if [[ -n $complexity || -n $(cat "./sequences/$align_file") ]]
 	then
 		if [[ ! -n $(cat "./phylogenies/$align_file.phylo_IQ-Tree.tre") ]]
@@ -101,14 +104,14 @@ then
 			fi
 		echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_IQ-Tree.log"
 	else
-		echo "warning: IQ-Tree failed to run for $align_file" 
+		echo "warning: IQ-Tree failed to run for $align_file"
 		#print something to the warning log
 	fi
 fi
-
-if [[ $phylo_tool = MPBoot ]] # change everything to MPBoot instead of IQ-Tree
+exit
+if [[ $phylo_tool = "MPBoot" ]] # change everything to MPBoot instead of IQ-Tree
 then
-	if [[ ! -n "./phylogenies/$align_file.phylo_MPBoot.log"]]
+	if [[ ! -n "./phylogenies/$align_file.phylo_MPBoot.log" ]]
         then
                 touch "./phylogenies/$align_file.phylo_MPBoot.log"
         else
@@ -126,11 +129,10 @@ then
                 fi
 
                         echo "name and location of newick string file: ./phylogenies/$align_file.phylo_MPBoot.tre" >> "./phylogenies/$align_file.phylo_MPBoot.log"
-		       	mpboot -s $(cat "./sequences/$align_file" -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_MPBoot.tre"
+		       	mpboot -s $(cat "./sequences/$align_file") -bb 1000 -o "$OUTGROUP" -pre "./phylogenies/$align_file.phylo_MPBoot.tre"
 			echo "command string: mpboot -s $(cat \"./sequences/$align_file\" -bb 1000 -o \"$OUTGROUP\" -pre \"./phylogenies/$align_file.phylo_MPBoot.tre\"" >> "./phylogenies/$align_file.phylo_MPBoot.log"
                 	echo "date the phylogeny was created: $date" >> "./phylogenies/$align_file.phylo_MPBoot.log"
         else
-		echo "warning: MPBoot failed to run for $align_file"
-                #print something to the warning log
+		echo "warning: MPBoot failed to run for $align_file" >> "./warning.log"
         fi
 fi
